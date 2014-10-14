dc38d36adf4bdde772ef7fce3876667620677dae
s14010 <s14010@std.it-college.ac.jp>
2014-10-14 19:22:48 +0900

    chap1演習 10/14

--- Ex1.java
+++ Ex1.java
@@ -10,10 +10,15 @@ class Ex1_0 extends Ex {
 
 class Ex1_2 extends Ex {
 	void execute(){
-		System.out.println("諸\n見\n里\n\n磨\n人");
+		System.out.println("諸\n見\n里\n磨\n人");
 	}
 }
 
+class Ex1_3 extends Ex {
+        void  execute(){
+                System.out.println("諸\n見\n里\n\n磨\n人");
+        }
+}
 public class Ex1 {
 	public static void main(String[] args){
 		Ex a;
@@ -21,5 +26,7 @@ public class Ex1 {
 		a.execute();
 		a = new Ex1_2();
 		a.execute();
+                a = new Ex1_3();
+                a.execute();
 	}
 }
